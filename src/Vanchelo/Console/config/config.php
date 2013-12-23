238036a1e03fdf84f678545833e1959c044353ec
vanchelo <npobolka@gmail.com>
2013-12-23 18:04:01 +0200

    Добавлен класс проверки прав доступа к консоли

--- src/Vanchelo/Console/config/config.php
+++ src/Vanchelo/Console/config/config.php
@@ -12,37 +12,36 @@ return new \Phalcon\Config([
     'whitelist' => 'whitelist',
     'blacklist' => 'blacklist',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Console routes filter
+    |--------------------------------------------------------------------------
+    |
+    | Set filter used for managing access to the console. By default, filter
+    | 'whitelist' allows only people from 'whitelist' array below.
+    |
+    */
 
-	/*
-	|--------------------------------------------------------------------------
-	| Console routes filter
-	|--------------------------------------------------------------------------
-	|
-	| Set filter used for managing access to the console. By default, filter
-	| 'whitelist' allows only people from 'whitelist' array below.
-	|
-	*/
+    'filter' => 'whitelist', // whitelist or blacklist
 
-	'filter' => 'whitelist', // whitelist or blacklist
+    /*
+    |--------------------------------------------------------------------------
+    | Enable console only for this locations
+    |--------------------------------------------------------------------------
+    |
+    | Addresses allowed to access the console. This array is used in
+    | 'whitelist' route filter. Nevertheless, this bundle should never
+    | get nowhere near your production servers, but who am I to tell you how
+    | to live your life :)
+    |
+    */
 
-	/*
-	|--------------------------------------------------------------------------
-	| Enable console only for this locations
-	|--------------------------------------------------------------------------
-	|
-	| Addresses allowed to access the console. This array is used in
-	| 'whitelist' route filter. Nevertheless, this bundle should never
-	| get nowhere near your production servers, but who am I to tell you how
-	| to live your life :)
-	|
-	*/
+    'whitelist' => [
+        '127.0.0.1',
+        '::1'
+    ],
 
-	'whitelist' => [
-		'127.0.0.1',
-		'::1'
-	],
-
-	'blacklist' => [
+    'blacklist' => [
 
     ],
 ]);
